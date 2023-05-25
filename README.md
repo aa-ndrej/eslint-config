9e4d63fd3093e1c5883b5a9e3b942be673ca1a13
Andrej Agapow <dev@agapow.de>
2023-05-25 18:52:31 +0200

    add basic project files

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # @aa-ndrej/eslint-config
+
+![GitHub package.json version](https://img.shields.io/github/package-json/v/aa-ndrej/eslint-config?style=for-the-badge)
---
+++ packages/eslint-config-base/index.js
@@ -0,0 +1,9 @@
+/** @type {import('eslint').ESLint.ConfigData} */
+module.exports = {
+	extends: ['eslint:recommended'],
+	reportUnusedDisableDirectives: true,
+
+	env: {
+		node: true,
+	}
+}
